bce21e3f48f91f3c2277bda8286447cf758ef67f
Nicolas Mesa <nicolasmesa@gmail.com>
2018-04-30 22:30:35 -0700

    Adding .vim and .vimrc to install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf "$DIR/.tmux.conf" ~/.tmux.conf
 ln -sf "$DIR/gitignore" ~/.gitignore
+ln -sf "$DIR/.vim" ~/.vim
+ln -sf "$DIR/.vimrc" ~/.vimrc
+
 git config --global core.excludesfile ~/.gitignore
